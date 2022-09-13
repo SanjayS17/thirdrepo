ef8d35b9d0cd1ac26457c193145183d94bfacb17
sanjay s <sanjays.xworkz@gmail.com>
2022-09-13 12:30:22 +0530

    natraj

---
+++ Array.java
@@ -0,0 +1,20 @@
+class Array{
+	public static void main(String[]manu1){
+		
+		
+		
+		
+	
+	int ref1=10;
+int ref2=9;
+int ref3=8;
+int[] manu={ref1,ref2,ref3};
+                      
+	
+	System.out.println(manu[2]);//
+	for(int manu=0;manu<=)
+	
+	
+	
+	
+}}
