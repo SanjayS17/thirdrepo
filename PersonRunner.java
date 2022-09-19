ca271baf7763eb5d1dfb9a919b8508802005488c
SanjayS17 <113328702+SanjayS17@users.noreply.github.com>
2022-09-19 08:17:53 +0530

    Add files via upload

---
+++ PersonRunner.java
@@ -0,0 +1,11 @@
+class PersonRunner
+{
+	public static void main(String[] working)
+	{
+		System.out.println("Enterd Company and Salary and Destination");
+		Person.Work("HP" ,30000);
+		Person.Work("Wipro",40000 ,"Developer");
+		
+	}
+	
+}
