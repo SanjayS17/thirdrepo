372dbf9b29bedda33e1ce58b5893ba13576249fd
SanjayS17 <113328702+SanjayS17@users.noreply.github.com>
2022-09-21 08:05:38 +0530

    Add files via upload

---
+++ CardRunner.java
@@ -0,0 +1,14 @@
+class CardRunner{
+	public static void main(String[] args)
+	{
+       Card.displaydetails();	
+       Card.type="credit card";
+       Card.height="13";
+       Card.width=28;
+       Card.price=1500;
+       String[]color={"Mirage Silver","Pine Green","Lake Blue","Grey"};
+       Card.color=color;
+	   Card.displaydetails();
+	}
+	
+	}
