efd084da3778087b87df82dec2843c6d8aacf4d1
SanjayS17 <113328702+SanjayS17@users.noreply.github.com>
2022-09-21 08:06:04 +0530

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,31 @@
+class Card{
+   
+       static String type;
+       static String height;  
+       static int width;
+       static double price;
+        static String[]color; 
+
+
+   static void displaydetails(){
+	
+       System.out.println(type);
+       System.out.println(height);  
+       System.out.println(width);
+       System.out.println(price);
+          if(color!=null){
+			  for (int position=0;position<color.length;position++)
+			  {
+				  String ref=color[position];
+				  System.out.println(ref+" "+position);
+			  }
+		  }
+		    else{
+				
+				System.out.println("array is null");
+			}
+   }
+      }
+                                      
+
+                        
