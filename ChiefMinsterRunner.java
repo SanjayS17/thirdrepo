bd12ede1e2972bf6b7e9a9159e08975520740c97
SanjayS17 <113328702+SanjayS17@users.noreply.github.com>
2022-09-23 08:33:01 +0530

    Add files via upload

---
+++ ChiefMinsterRunner.java
@@ -0,0 +1,48 @@
+	class ChiefMinsterRunner{
+	
+	public static void main(String[] votes){
+	
+	System.out.println("Printing Information");
+	
+	String ref1=votes[0];
+	System.out.println("index[0]="+ref1);
+	
+	String ref2=votes[1];
+	System.out.println("index[1]="+ref2);
+	
+	String ref3=votes[2];
+	System.out.println("index[2]="+ref3);
+	
+	String ref4=votes[3];
+	System.out.println("index[3]="+ref4);
+	
+	String ref5=votes[4];
+	System.out.println("index[4]="+ref5);
+	
+	String ref6=votes[5];
+	System.out.println("index[5]="+ref6);
+	
+	String ref7=votes[6];
+	System.out.println("index[6]="+ref7);
+	
+	String ref8=votes[7];
+	System.out.println("index[7]="+ref8);
+	
+	String ref9=votes[8];
+	System.out.println("index[8]="+ref9);
+	
+	String ref10=votes[9];
+	System.out.println("index[9]="+ref10);
+	 
+	
+	 
+	 
+	 System.out.println("Information"+votes.length);
+	}	
+	
+	
+}
+	
+	
+	
+	
